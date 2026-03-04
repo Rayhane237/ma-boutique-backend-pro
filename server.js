2396ef60df2241fb5d128f4beecae41291e88d7e
Rayhane <rayhane775@gmail.com>
2026-03-04 18:50:14 +0100

    cors:my actual front

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ const app = express() // creates your server
 
 // ── Middleware ──
 // These 2 lines run on EVERY request before it hits your routes
-app.use(cors())             // allows React (port 5173) to talk to Express (port 5000)
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://ma-boutiqueadeline-aaqdiwj8n-tliba-raihanes-projects.vercel.app/'  // replace with your actual frontend URL
+  ],
+  credentials: true
+}))           // allows React (port 5173) to talk to Express (port 5000) ,and my deployed frontend to talk to my deployed backend
 app.use(express.json())     // lets Express read JSON from request bodies
 
 // ── Routes ──
@@ -21,11 +27,12 @@ app.use('/api/products', productRoutes)
 app.use("/api/messages", messageRoutes)
 
 // ── Connect to MongoDB then start server ──
-// We connect to DB FIRST, only then open the server
+// Connect to DB FIRST, only then open the server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('✅ MongoDB connected')
-    app.listen(5000, () => console.log('✅ Server running on port 5000'))
+    console.log(' MongoDB connected')
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
   })
-  .catch((err) => console.log('❌ Connection failed:', err))
+  .catch((err) => console.log(' Connection failed:', err))
